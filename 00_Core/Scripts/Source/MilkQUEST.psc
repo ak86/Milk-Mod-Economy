ak86/Milk-Mod-Economy

commit 33a6bdd9304f9978a48927bcebf10186d6118454

1

00_Core/Scripts/Source/MilkQUEST.psc
@@ -864,6 +864,8 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 	Bool ZaZGag = zbf.IsWearingZaZGag(akActor)
 	Bool ZaZGagOpen = zbf.IsWearingZaZGag_Open(akActor)
 	
+	String akActorGender = akActorSex(akActor)
+	
 	String anivar = ""
 	if DDBeltOpen == true					;--vaginal probe
 		anivar = anivar + "_02"
@@ -973,7 +975,7 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 
 		Utility.Wait( 5.0 )															;wait for actor to "sit"
 		If IsMilkingBlocked == false && SLSDBra == false
-			If MilkStory && akActor == PlayerREF && akActor.GetLeveledActorBase().GetSex() == 1
+			If MilkStory && akActor == PlayerREF && akActorGender != "Male"
 				StoryDisplay(0,1,FirstTimeStory)
 			EndIf
 			
@@ -988,7 +990,7 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 						akActor.additem(ZaZMoMSuctionCups, 1, true)
 						akActor.equipitem(ZaZMoMSuctionCups, true, true)
 					elseif !akActor.IsEquipped(cuirass)
-						if akActorSex(akActor) == "Futa"
+						if akActorGender == "Futa"
 							akActor.additem(MilkCuirassFuta, 1, true)
 							akActor.equipitem(MilkCuirassFuta, true, true)
 						else
@@ -1002,7 +1004,7 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 					akActor.additem(ZaZMoMSuctionCups, 1, true)
 					akActor.equipitem(ZaZMoMSuctionCups, true, true)
 				else
-					if akActorSex(akActor) == "Futa"
+					if akActorGender == "Futa"
 						akActor.additem(MilkCuirassFuta, 1, true)
 						akActor.equipitem(MilkCuirassFuta, true, true)
 					else
@@ -1023,11 +1025,11 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 			|| MilkingEquipment.find(cuirass.getname()) >= 0\
 			|| SLSDBra == true
 				Mode = 2
-			ElseIf akActor.GetItemCount(MilkCuirassFuta) > 0 && akActorSex(akActor) == "Futa"
+			ElseIf akActor.GetItemCount(MilkCuirassFuta) > 0 && akActorGender == "Futa"
 				akActor.equipitem(MilkCuirassFuta, true, true)
 				hasInventoryMilkCuirassFuta = true
 				Mode = 2
-			ElseIf akActor.GetItemCount(MilkCuirass) > 0 && akActorSex(akActor) != "Futa"
+			ElseIf akActor.GetItemCount(MilkCuirass) > 0 && akActorGender != "Futa"
 				akActor.equipitem(MilkCuirass, true, true)
 				hasInventoryMilkCuirass = true
 				Mode = 2
@@ -1040,7 +1042,7 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 				Mode = 3
 				int soundInstance02 = TakeHoldSound.Play(akActor)
 				Utility.Wait( 5.0 )													;wait for possession sound to play
-				If MilkStory && akActor == PlayerREF && akActor.GetLeveledActorBase().GetSex() == 1
+				If MilkStory && akActor == PlayerREF && akActorGender != "Male"
 					if StringUtil.Find(cuirass.getname(), "Hermaeus Mora" ) >= 0 || StringUtil.Find(cuirass.getname(), "HM Priestess" ) >= 0
 						StoryDisplay(0,3,FirstTimeStory)
 					elseif StringUtil.Find(cuirass.getname(), "Living Arm" ) >= 0
@@ -1053,11 +1055,11 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 			Else
 				akActor.UnequipItem(cuirass, false, true)
 			EndIf
-		ElseIf akActor.GetItemCount(MilkCuirassFuta) > 0 && akActorSex(akActor) == "Futa"
+		ElseIf akActor.GetItemCount(MilkCuirassFuta) > 0 && akActorGender == "Futa"
 			akActor.equipitem(MilkCuirassFuta, true, true)
 			hasInventoryMilkCuirassFuta = true
 			Mode = 2
-		ElseIf akActor.GetItemCount(MilkCuirass) > 0 && akActorSex(akActor) != "Futa"
+		ElseIf akActor.GetItemCount(MilkCuirass) > 0 && akActorGender != "Futa"
 			akActor.equipitem(MilkCuirass, true, true)
 			hasInventoryMilkCuirass = true
 			Mode = 2
@@ -1111,7 +1113,7 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 		endif
 	endif
 	
-	while (((akActor.GetSitState() <= 3 && akActor.GetSitState() > 0) || akActor.GetParentCell() != PlayerREF.GetParentCell())\
+	while (((akActor.GetSitState() <= 3 && akActor.GetSitState() > 0) || akActor.IsInLocation(PlayerREF.getCurrentLocation()))\
 			|| (MilkCnt >= 1 && Mode > 0 && ((PainCnt <= (((MaidLevel+2)*2)-((MaidLevel+2)*2)/10)) || PainKills)))\
 			&& (akActor.HasSpell(BeingMilkedPassive) || !IsMilkMaid)
 
@@ -1148,10 +1150,10 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 			endif
 			sendVibrationEvent("FeedingStage", akActor, mpas, MilkingType)
 
-			if Plugin_SlSW && akActor == PlayerREF && !DisableSkoomaLactacid
+			if Plugin_SlSW && akActor == PlayerREF && IsMilkMaid == true && !DisableSkoomaLactacid
 				akActor.equipitem(Game.GetFormFromFile(0x57A7A, "Skyrim.esm"),false,true)
 			endif
-			while duration < Feeding_Duration && (((akActor.GetSitState() <= 3 && akActor.GetSitState()) || akActor.GetParentCell() != PlayerREF.GetParentCell())|| Mode != 0) && (akActor.HasSpell(BeingMilkedPassive) || !IsMilkMaid)
+			while duration < Feeding_Duration && (((akActor.GetSitState() <= 3 && akActor.GetSitState()) || akActor.IsInLocation(PlayerREF.getCurrentLocation()))|| Mode != 0) && (akActor.HasSpell(BeingMilkedPassive) || !IsMilkMaid)
 				LactacidCnt = LactacidCnt + (MaidLevel+2)/Feeding_Duration
 				Utility.Wait(1.0)
 				duration = duration + 1
@@ -1423,7 +1425,7 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 		
 	endwhile
 
-	;debug.Notification("milking done." +akActorSex(akActor)+ " "+isPregnant(akActor))
+	;debug.Notification("milking done." +akActorGender+ " "+isPregnant(akActor))
 	sendVibrationEvent("StopMilkingMachine", akActor, mpas, MilkingType)
 
 ;-----------------------Milking done
@@ -1451,13 +1453,13 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 					akActor.RemoveItem(MilkCuirassFuta, 1, true)
 				endif
 			endif
-			If MilkStory && akActor == PlayerREF && akActor.GetLeveledActorBase().GetSex() == 1
+			If MilkStory && akActor == PlayerREF && akActorGender != "Male"
 				Utility.Wait(5.0)											;wait for actor to get off milking pump
 				StoryDisplay(1,1,FirstTimeStory)
 			EndIf
 			Debug.SendAnimationEvent(akActor,"IdleForceDefaultState")
 		elseif Mode == 3
-			If MilkStory && akActor == PlayerREF && akActor.GetLeveledActorBase().GetSex() == 1
+			If MilkStory && akActor == PlayerREF && akActorGender != "Male"
 				if StringUtil.Find(cuirass.getname(), "Hermaeus Mora" ) >= 0 || StringUtil.Find(cuirass.getname(), "HM Priestess" ) >= 0
 					StoryDisplay(1,3,FirstTimeStory)
 				elseif StringUtil.Find(cuirass.getname(), "Living Arm" ) >= 0
@@ -1519,11 +1521,11 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 					if StorageUtil.GetIntValue(akActor, "MME.MilkMaid.IsSlave") == 1 || MILKSlave.find(akActor) != -1
 						StorageUtil.AdjustFloatValue(akActor, "MME.MilkMaid.MilkingContainerCumsSUM", cumcount)
 					elseif Mode == 0 || Mode == 2
-						if akActorSex(akActor) == "Male" 
+						if akActorGender == "Male" 
 							PlayerREF.AddItem(MME_Cums.GetAt(1), cumcount + StorageUtil.GetFloatValue(akActor,"MME.MilkMaid.MilkingContainerCumsSUM") as int)
-						elseif akActorSex(akActor) == "Female" 
+						elseif akActorGender == "Female" 
 							PlayerREF.AddItem(MME_Cums.GetAt(0), cumcount + StorageUtil.GetFloatValue(akActor,"MME.MilkMaid.MilkingContainerCumsSUM") as int)
-						elseif akActorSex(akActor) == "Futa"
+						elseif akActorGender == "Futa"
 							int futamilk = Utility.RandomInt(0, cumcount + StorageUtil.GetFloatValue(akActor,"MME.MilkMaid.MilkingContainerCumsSUM") as int)
 							PlayerREF.AddItem(MME_Cums.GetAt(3), futamilk)
 							PlayerREF.AddItem(MME_Cums.GetAt(2), cumcount + StorageUtil.GetFloatValue(akActor,"MME.MilkMaid.MilkingContainerCumsSUM") as int - futamilk)

00_Core/Scripts/Source/MilkCheckForSprigganScr.psc
@@ -1,78 +1,84 @@
 Scriptname MilkCheckForSprigganScr extends ReferenceAlias
-;script running only on player
 
 MilkQUEST Property MilkQ Auto
 
 ;----------------------------------------------------------------------------
 Event OnObjectEquipped( Form akBaseObject, ObjectReference akReference)
-	Actor PlayerREF = MilkQ.PlayerREF
-	if akBaseObject as Armor\
-	&& (StringUtil.Find(akBaseObject.getname(), "Spriggan Armor" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "Spriggan Host" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "Living Arm" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "Hermaeus Mora" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "HM Priestess" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "Tentacle Parasite" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "Tentacle Armor" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "Dwemer milking device" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "Cow Harness" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "Milking Cuirass" ) >= 0\
-	|| 	StringUtil.Find(akBaseObject.getname(), "Milker" ) >= 0\
-	|| 	MilkQ.MilkingEquipment.find(akBaseObject.getname()) >= 0\
-	|| 	MilkQ.BasicLivingArmor.find(akBaseObject.getname()) >= 0\
-	|| 	MilkQ.ParasiteLivingArmor.find(akBaseObject.getname()) >= 0)
+	Actor akActor = GetActorRef()
+	String akArmorName = akBaseObject.getname()
+	if (StringUtil.Find(akArmorName, "Spriggan Armor" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "Spriggan Host" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "Living Arm" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "Hermaeus Mora" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "HM Priestess" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "Tentacle Parasite" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "Tentacle Armor" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "Dwemer milking device" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "Cow Harness" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "Milking Cuirass" ) >= 0\
+	|| 	StringUtil.Find(akArmorName, "Milker" ) >= 0\
+	|| 	MilkQ.MilkingEquipment.find(akArmorName) >= 0\
+	|| 	MilkQ.BasicLivingArmor.find(akArmorName) >= 0\
+	|| 	MilkQ.ParasiteLivingArmor.find(akArmorName) >= 0)
 
-		If MilkQ.MILKmaid.find(PlayerREF) == -1
-			MilkQ.AssignSlot(PlayerREF)
+		If MilkQ.MILKmaid.find(akActor) == -1
+			MilkQ.AssignSlot(akActor)
 		EndIf
 
-		if PlayerREF.GetActorBase().GetSex() == 1
-			If (StringUtil.Find(akBaseObject.getname(), "Spriggan Armor" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Spriggan Host" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Living Arm" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Hermaeus Mora" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "HM Priestess" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Tentacle Parasite" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Tentacle Armor" ) >= 0\
-			|| MilkQ.BasicLivingArmor.find(akBaseObject.getname()) >= 0)\
-			|| MilkQ.ParasiteLivingArmor.find(akBaseObject.getname()) >= 0
-				Debug.Notification(akBaseObject.getname() + " attaches to your breasts")
-				PlayerREF.AddSpell( MilkQ.MilkForSprigganPassive, false )
-				If StorageUtil.GetFloatValue(PlayerREF,"MME.MilkMaid.LactacidCount") < 1
-					StorageUtil.SetFloatValue(PlayerREF,"MME.MilkMaid.LactacidCount", 1)
+		if akActor.GetLeveledActorBase().GetSex() == 1 && MilkQ.MILKmaid.find(akActor) != -1
+			If (StringUtil.Find(akArmorName, "Spriggan Armor" ) >= 0\
+			|| StringUtil.Find(akArmorName, "Spriggan Host" ) >= 0\
+			|| StringUtil.Find(akArmorName, "Living Arm" ) >= 0\
+			|| StringUtil.Find(akArmorName, "Hermaeus Mora" ) >= 0\
+			|| StringUtil.Find(akArmorName, "HM Priestess" ) >= 0\
+			|| StringUtil.Find(akArmorName, "Tentacle Parasite" ) >= 0\
+			|| StringUtil.Find(akArmorName, "Tentacle Armor" ) >= 0\
+			|| MilkQ.BasicLivingArmor.find(akArmorName) >= 0)\
+			|| MilkQ.ParasiteLivingArmor.find(akArmorName) >= 0
+				if akActor == MilkQ.PlayerREf
+					Debug.Notification(akArmorName + " attaches to your breasts")
+				endif
+				akActor.AddSpell( MilkQ.MilkForSprigganPassive, false )
+				If StorageUtil.GetFloatValue(akActor,"MME.MilkMaid.LactacidCount") < 1
+					StorageUtil.SetFloatValue(akActor,"MME.MilkMaid.LactacidCount", 1)
 				EndIf
 			Else
-				Debug.Notification( "The milking cups attach to your breasts, ready to milk you" )
+				if akActor == MilkQ.PlayerREf
+					Debug.Notification( "The milking cups attach to your breasts, ready to milk you" )
+				endif
 			EndIf
 		EndIf
 	EndIf
-	if MilkQ.PlayerREF.GetWornForm(Armor.GetMaskForSlot(32)) != None && MilkQ.MilkLeakWearArm == False && MilkQ.PlayerREF.HasSpell(MilkQ.MilkLeak)
-		MilkQ.PlayerREF.RemoveSpell(MilkQ.MilkLeak)
+	if akActor.GetWornForm(Armor.GetMaskForSlot(32)) != None && MilkQ.MilkLeakWearArm == False && akActor.HasSpell(MilkQ.MilkLeak)
+		akActor.RemoveSpell(MilkQ.MilkLeak)
 	endif
 EndEvent
 
 Event OnObjectUnequipped( Form akBaseObject, ObjectReference akReference )
-	Actor PlayerREF = MilkQ.PlayerREF
-	if PlayerREF.GetActorBase().GetSex() == 1
-		if akBaseObject as Armor
-			if StringUtil.Find(akBaseObject.getname(), "Spriggan Armor" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Spriggan Host" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Living Arm" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Hermaeus Mora" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "HM Priestess" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Tentacle Parasite" ) >= 0\
-			|| StringUtil.Find(akBaseObject.getname(), "Tentacle Armor" ) >= 0\
-			|| MilkQ.BasicLivingArmor.find(akBaseObject.getname()) >= 0\
-			|| MilkQ.ParasiteLivingArmor.find(akBaseObject.getname()) >= 0
-				Debug.Notification( "You are free from " + akBaseObject.getname() )
-				PlayerREF.RemoveSpell( MilkQ.MilkForSprigganPassive )
-			ElseIf StringUtil.Find(akBaseObject.getname(), "Dwemer milking device" ) >= 0\
-				|| StringUtil.Find(akBaseObject.getname(), "Cow Harness" ) >= 0\
-				|| StringUtil.Find(akBaseObject.getname(), "Milking Cuirass" ) >= 0\
-				|| StringUtil.Find(akBaseObject.getname(), "Milker" ) >= 0\
-				|| MilkQ.MilkingEquipment.find(akBaseObject.getname()) >= 0
+	Actor akActor = GetActorRef()
+	String akArmorName = akBaseObject.getname()
+	if MilkQ.MILKmaid.find(akActor) != -1
+		if StringUtil.Find(akArmorName, "Spriggan Armor" ) >= 0\
+		|| StringUtil.Find(akArmorName, "Spriggan Host" ) >= 0\
+		|| StringUtil.Find(akArmorName, "Living Arm" ) >= 0\
+		|| StringUtil.Find(akArmorName, "Hermaeus Mora" ) >= 0\
+		|| StringUtil.Find(akArmorName, "HM Priestess" ) >= 0\
+		|| StringUtil.Find(akArmorName, "Tentacle Parasite" ) >= 0\
+		|| StringUtil.Find(akArmorName, "Tentacle Armor" ) >= 0\
+		|| MilkQ.BasicLivingArmor.find(akArmorName) >= 0\
+		|| MilkQ.ParasiteLivingArmor.find(akArmorName) >= 0
+			if akActor == MilkQ.PlayerREf
+				Debug.Notification( "You are free from " + akArmorName )
+			endif
+			akActor.RemoveSpell( MilkQ.MilkForSprigganPassive )
+		ElseIf akActor.GetLeveledActorBase().GetSex() == 1 && StringUtil.Find(akArmorName, "Dwemer milking device" ) >= 0\
+			|| StringUtil.Find(akArmorName, "Cow Harness" ) >= 0\
+			|| StringUtil.Find(akArmorName, "Milking Cuirass" ) >= 0\
+			|| StringUtil.Find(akArmorName, "Milker" ) >= 0\
+			|| MilkQ.MilkingEquipment.find(akArmorName) >= 0
+			if akActor == MilkQ.PlayerREf
 				Debug.Notification( "The milking cups detach from your breasts" )
-			EndIf
+			endif
 		EndIf
 	EndIf
 EndEvent

commit 5c4b3487d811747864d104dbf497d3029bdf02eb

fix 'endless milking' bug
Symptom:
  Trying to stand up while using the milking machine or running out of milk
  would not stop the milking cycle, it would continue endlessly.

This bug was introduced with commit 33a6bdd9304f9978a48927bcebf10186d6118454

00_Core/Scripts/Source/MilkQUEST.psc
@@ -1113,7 +1113,7 @@ Function Milking(Actor akActor, int i, int Mode, int MilkingType)
 		endif
 	endif
 	
-	while (((akActor.GetSitState() <= 3 && akActor.GetSitState() > 0) || akActor.IsInLocation(PlayerREF.getCurrentLocation()))\
+	while (((akActor.GetSitState() <= 3 && akActor.GetSitState() > 0) || akActor.GetParentCell() != PlayerREF.GetParentCell())\
 			|| (MilkCnt >= 1 && Mode > 0 && ((PainCnt <= (((MaidLevel+2)*2)-((MaidLevel+2)*2)/10)) || PainKills)))\
 			&& (akActor.HasSpell(BeingMilkedPassive) || !IsMilkMaid)
 